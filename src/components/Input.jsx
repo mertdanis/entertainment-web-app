096d36166ba5d8980d9a31b1481f45afa400e05c
M. D <mertdanis@outlook.com>
2023-10-17 19:07:04 +0300

    update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,10 @@
 function Input({ onChange, placeHolder }) {
   return (
-    <div className="flex items-center  justify-center gap-6 col-start-1 col-end-6">
+    <div className="flex items-center relative  justify-center gap-6 col-start-1 col-end-6">
       <img src="/public/assets/imgs/icon-search.svg" alt="search-icon" />
       <input
         onChange={onChange}
-        className=" text-headingM focus:pb-[14px] border-mainLightBlue caret-mainRed font-light p-3  focus:border-b-[1px] border-0 outline-0  w-full  bg-transparent text-white opacity-50"
+        className=" text-headingM focus:pb-[14px] border-mainLightBlue caret-mainRed font-light p-3  focus:border-b-[1px] border-0 outline-0  w-full  bg-transparent text-mainWhite opacity-50"
         type="text"
         placeholder={placeHolder}
       />
